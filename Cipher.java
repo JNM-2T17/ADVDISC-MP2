2a2b77a36268e3d438ff3d22f37d49a8a5326b93
John Joseph Andres <andres_johnjoseph@yahoo.com>
2015-10-28 14:10:44 +0800

    yay
    jojo

---
+++ Cipher.java
@@ -0,0 +1,47 @@
+public class Cipher
+{
+	private int cipherDim;
+
+	public Cipher(int x)
+	{
+
+	}
+
+	public String encipher(String s, Matrix m)
+	{
+		int x;
+
+		x = (int)s.CharAt(index);
+	}
+
+	private int[][] findIndependent(String s1, String s2)
+	{
+
+	}
+
+	private boolean isIndependent(int[][] intArray)
+	{
+
+	}
+
+	public String decipher(String s, Matrix m)
+	{
+
+	}
+
+	public Matrix deriveCipher(String s1, String s2)
+	{
+
+	}
+
+	private Matrix convertToMatrix(String s)
+	{
+		
+	}
+
+	public String convertToString(Matrix m)
+	{
+ 
+	}
+
+}
---
+++ ModArith.java
@@ -0,0 +1,16 @@
+public class ModArith
+{
+	public static int modulo(int n, int mod)
+	{
+		if( n >= 0)
+			return n/mod;
+		else if( n < 0)
+			return (mod + n)/mod;
+	}
+
+	public static int modInverse(int x,int y)
+	{
+		//si Marc ay _ _ _ _ _ _!
+
+	}
+}
