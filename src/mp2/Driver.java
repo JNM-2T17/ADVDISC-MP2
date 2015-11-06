267e0faf8e968a67adacdd2e507e718b344afdeb
Austin Fernandez <ryanaustinf@yahoo.com>
2015-11-06 15:57:24 +0800

    Javadoc

--- src/mp2/Driver.java
+++ src/mp2/Driver.java
@@ -29,25 +29,25 @@ public class Driver {
 		m = FileManager.readCipher(cipher_filename);
 		System.out.println(m);
 		
-		Cipher c = new Cipher(3);
+		Cipher c = new Cipher(4);
 
-		System.out.print("Enter ciphertext: ");
-		String s2 = sc.nextLine();
+		// System.out.print("Enter ciphertext: ");
+		// String s2 = sc.nextLine();
 
-		System.out.print("Enter plaintext: ");
-		String s1 = sc.nextLine();
-		//String s1;
+		// System.out.print("Enter plaintext: ");
+		// String s1 = sc.nextLine();
+		String s1;
 
 		// FileManager.writeText(s1,plain_filename);
-		//s1 = FileManager.readText(plain_filename);
+		s1 = FileManager.readText(plain_filename);
 		// System.out.println("Enciphering " + s1);
 
-		m = c.deriveCipher(s1,s2);
-		System.out.println(m);
-		/*FileManager.writeCipher(m,cipher_filename);*/
+		// m = c.deriveCipher(s1,s2);
+		// System.out.println(m);
+		// FileManager.writeCipher(m,cipher_filename);
 
 		String enc = c.encipher(s1,m);
-		System.out.println("Enciphered: " + enc);
+		// System.out.println("Enciphered: " + enc);
 		
 		FileManager.writeText(enc,ciphertext_filename);
 		String enc2 = FileManager.readText(ciphertext_filename);
@@ -55,7 +55,7 @@ public class Driver {
 		// System.out.println("Deciphering " + enc);
 		
 		String dec = c.decipher(enc2,m);
-		System.out.println("Deciphered: " + dec);
+		// System.out.println("Deciphered: " + dec);
 		FileManager.writeText(dec,plain_filename);
 		// int[][] cInt = new int[][]{
 		// 	{4,1,20},
