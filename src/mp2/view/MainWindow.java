be5b1245ba1ae837dffab66256ac10b31a5784d4
Austin Fernandez <ryanaustinf@yahoo.com>
2015-11-15 11:11:34 +0800

    More UI Tweaks.

--- src/mp2/view/MainWindow.java
+++ src/mp2/view/MainWindow.java
@@ -53,7 +53,7 @@ public class MainWindow extends JFrame {
 		setBackground(Color.WHITE);
 		setTitle("ADVDISC MP2");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(300,500);
+		setSize(375,500);
 		setLocationRelativeTo(null);
 
 		menuBar = new JMenuBar();
@@ -63,6 +63,7 @@ public class MainWindow extends JFrame {
 		setJMenuBar(menuBar);
 
 		mnActions = new JMenu("Actions");
+		mnActions.setForeground(new Color(200,200,255));
 		menuBar.add(mnActions);
 
 		itemEncipher = new JMenuItem("Encipher");
@@ -75,6 +76,7 @@ public class MainWindow extends JFrame {
 		mnActions.add(itemDerive);
 
 		mnHelp = new JMenu("Help");
+		mnHelp.setForeground(new Color(200,200,255));
 		menuBar.add(mnHelp);
 
 		itemInstructions = new JMenuItem("Instructions");
@@ -84,40 +86,40 @@ public class MainWindow extends JFrame {
 		mnHelp.add(itemCredits);
 
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new AGBLayout());
 
 		lblWelcome = new JLabel("WELCOME!");
-		lblWelcome.setFont(new Font("Serif", Font.PLAIN, 28));
+		lblWelcome.setFont(new Font("Verdana", Font.PLAIN, 28));
 		lblWelcome.setHorizontalAlignment(SwingConstants.CENTER);
 		AGBLayout.addComp(contentPane,lblWelcome,0,0,1,1,100,100
 							,GridBagConstraints.CENTER,GridBagConstraints.NONE);
 
 		lblWhatDoYou = new JLabel("What do you wish to do today?");
 		lblWhatDoYou.setHorizontalAlignment(SwingConstants.CENTER);
-		lblWhatDoYou.setFont(new Font("Serif", Font.PLAIN, 18));
+		lblWhatDoYou.setFont(new Font("Verdana", Font.PLAIN, 18));
 		AGBLayout.addComp(contentPane,lblWhatDoYou,0,1,1,1,100,100
 							,GridBagConstraints.CENTER,GridBagConstraints.NONE);
 
 		btnCipher = new JButton("ENCIPHER");
 		btnCipher.setFont(new Font("Verdana", Font.PLAIN, 20));
-		AGBLayout.addComp(contentPane,btnCipher,0,2,1,2,100,100,10,10,10,10
+		AGBLayout.addComp(contentPane,btnCipher,0,2,1,2,100,100,10,10,5,10
 							,GridBagConstraints.CENTER,GridBagConstraints.BOTH);
 
 		btnDecipher = new JButton("DECIPHER");
 		btnDecipher.setFont(new Font("Verdana", Font.PLAIN, 20));
-		AGBLayout.addComp(contentPane,btnDecipher,0,4,1,2,100,100,10,10,10,10
+		AGBLayout.addComp(contentPane,btnDecipher,0,4,1,2,100,100,5,10,5,10
 							,GridBagConstraints.CENTER,GridBagConstraints.BOTH);
 
 		btnDerive = new JButton("DERIVE");
 		btnDerive.setFont(new Font("Verdana", Font.PLAIN, 20));
-		AGBLayout.addComp(contentPane,btnDerive,0,6,1,2,100,100,10,10,10,10
+		AGBLayout.addComp(contentPane,btnDerive,0,6,1,2,100,100,5,10,10,10
 							,GridBagConstraints.CENTER,GridBagConstraints.BOTH);
 
 
-		lblCredits = new JLabel(" (c) Developed by Amadora, Andres,Fernandez, and Syfu");
-		lblCredits.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		lblCredits = new JLabel("<html>&copy; Amadora, Andres, Fernandez, Syfu</html>");
+		lblCredits.setFont(new Font("Sans Serif", Font.PLAIN, 12));
 		lblCredits.setHorizontalAlignment(SwingConstants.CENTER);
 		AGBLayout.addComp(contentPane,lblCredits,0,8,1,1,100,100
 							,GridBagConstraints.EAST,GridBagConstraints.NONE);
